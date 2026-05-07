a4c772cb72751ff91df0a535cba98ae98a77945d
vinhem <trumvinh85@gmail.com>
2026-05-08 00:06:48 +0700

    CN Thanh Toan Bang MQR

--- ThuongMaiDienTu/app/Http/Controllers/Admin/CartController.php
+++ ThuongMaiDienTu/app/Http/Controllers/Admin/CartController.php
@@ -5,39 +5,31 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-use App\Models\Product;
-
 class CartController extends Controller
 {
     /**
-     * Hiển thị giỏ hàng.
+     * Hiển thị giỏ hàng (trong Admin).
      */
     public function index()
     {
-        // Lấy 3 sản phẩm đầu tiên từ database để giả lập giỏ hàng
-        $products = Product::limit(3)->get();
-        
-        $cartItems = $products->map(function($product) {
-            return [
-                'id' => $product->product_id,
-                'name' => $product->name,
-                'price' => (int)$product->base_price,
-                'quantity' => rand(1, 2),
-                'stock' => 10,
-                'selected' => true,
-                'image' => $product->thumbnail,
-                'url' => '#'
-            ];
-        });
-
-        return view('frontend.cart.shoppingcart', compact('cartItems'));
+        return view('frontend.cart.shoppingcart');
     }
 
     /**
-     * Hiển thị trang tính phí vận chuyển.
+     * Hiển thị trang tính phí vận chuyển (trong Admin).
      */
     public function shipping()
     {
         return view('frontend.cart.ShippingCosts');
+    }
+
+    public function pay()
+    {
+        return view('frontend.cart.pay');
+    }
+
+    public function ai()
+    {
+        return view('frontend.cart.maQR');
     }
 }
