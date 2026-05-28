6a3283f40c453f85f3e8d0725e671e141145b1dd
DangNguyen <dn1275102@gmail.com>
2026-05-28 08:17:05 +0700

    Trien khai seeder du lieu mau cho phieu kiem ke kho

--- ThuongMaiDienTu/database/seeders/DatabaseSeeder.php
+++ ThuongMaiDienTu/database/seeders/DatabaseSeeder.php
@@ -24,6 +24,7 @@ class DatabaseSeeder extends Seeder
             VideoSeeder::class,
             ProductComboSeeder::class,
             InventoryMovementSeeder::class,
+            InventoryAuditSeeder::class,
         ]);
     }
 }
